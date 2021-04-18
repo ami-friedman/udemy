7268c068582d72db80cc76669ada30c38672da8c
Ami Friedman <ami-friedman@gmail.com>
2021-04-18 13:53:27 +0300

    as

--- udemy-js/ColorGame/colorgame.js
+++ udemy-js/ColorGame/colorgame.js
@@ -18,7 +18,6 @@ function init() {
 
 function setupListeners() {
     for (var i = 0; i < squares.length; i++) {
-
         squares[i].addEventListener("click", function () {
             if (this.style.backgroundColor === colorToGuessDisplay.textContent) {
                 feedbackText.textContent = "Correct!";
